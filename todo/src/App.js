23bd50f1514bb0e5db9226e7a7a055cae2576e25
Q7 <sihle@thedigitalacademy.co.za>
2021-05-04 14:17:30 +0200

    Working on displaying items

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,18 +1,44 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Item from "./components/ItemsList/ItemsList";
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = { todo: '' } 
+  }
 
-function App() {
-  return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      
-        <div className="flex-shrink-0">
+  handleChange = ({target}) => {
+    this.setState({todo: target.value});
+  }
+
+  addTodo= () => {
+    console.log(this.state.todo);
+    //  items.push(this.state.todo)
+  }
+  
+  render() {
+    return (
+      <div className="p-6 max-w-sm mx-auto  items-center">
+        <h1 className="text-3xl md:text-4xl font-medium mb-2">TODO APP</h1>
+
+        <div className="mb-3 flex pt-0">
+          <input
+            type="search"
+            placeholder="Enter todo"
+            onChange={ this.handleChange }
+            className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring w-full"
+          />
+          <button
+            className="bg-blue-500 text-white rounded font-bold py-2 px-4"
+            onClick={ this.addTodo }
+          >
+            Add
+          </button>
         </div>
-   <div>
-    <div className="text-xl font-medium text-black">ChitChat</div>
-    <p className="text-gray-500">You have a new message!</p>
-  </div>
-</div>
-
-  );
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ todo/src/components/ItemsList/ItemsList.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// const ItemsList = ({items}) => {
+//     return(
+//         <div>
+//             {
+//                 items.map((item) => <p>{ item }</p>)
+//             }
+//         </div>
+//     );
+// } 
+
+
+// export default ItemsList
